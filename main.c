a46427a4148c1c28bfc422dfc00d194561636005
tilda.niva@gmail.com <tilda.niva@gmail.com>
2025-08-04 11:03:01 +0200

    first

--- main.c
+++ main.c
@@ -5,9 +5,52 @@ int main()
 {
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
-
     
-    // Your code goes here
+    int dice_rolls[100];
+    for (int i=0; i<100; i++){
+        dice_rolls[i] =(rand()%6) +1;
+    }
+    int one = 0;
+    int two = 0;
+    int three = 0;
+    int four = 0;
+    int five = 0;
+    int six = 0;
+    int sum = 0;
 
+    for (int i = 0; i<100; i++){
+        if (dice_rolls[i] == 1){
+            one++;
+        }
+        if(dice_rolls[i] == 2){
+            two++;
+        }
+         if(dice_rolls[i] == 3){
+            three++;
+        }
+         if(dice_rolls[i] == 4){
+            four++;
+        }
+         if(dice_rolls[i] == 5){
+            five++;
+        }
+         if(dice_rolls[i] == 6){
+            six++;
+        }
+        sum+=dice_rolls[i];
+    }
+    printf("%d\n", one);
+    printf("%d\n", two);
+    printf("%d\n", three);
+    printf("%d\n", four);
+    printf("%d\n", five);
+    printf("%d\n", six);
+    printf("%d\n", sum);
+    printf("%.1f\n", sum/100.0 );
+
+
+
+    // Your code goes here
+    
     return 0;
 }
